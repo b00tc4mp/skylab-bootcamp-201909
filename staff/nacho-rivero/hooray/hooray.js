41c6393baa7e6387b233daf86154abb80d151049
RivannaDev <51270972+rivannadev@users.noreply.github.com>
2019-10-11 15:53:38 +0200

    hooray

--- staff/nacho-rivero/hooray/hooray.js
+++ staff/nacho-rivero/hooray/hooray.js
@@ -33,3 +33,50 @@ Hooray.prototype.push = function() {
 
 	return this.length;
 };
+
+/**
+ * The pop method removes the last element of an
+ * array, and returns that element.
+ * @param {Arguments} arguments The object to pop elements from.
+ * 
+ * @returns {last} Returns the argument without the last element.
+ * 
+ * 
+ */
+
+Hooray.prototype.pop = function() {
+
+	var last = this[this.length - 1];
+ 
+	delete this[this.length - 1];
+
+	this.length = this.length - 1;
+ 
+	return last; 
+ 
+ }
+
+ /**
+ * Map method creates a new array with the results of the function 
+ * callback applied to each one of its elements.
+ * @param {Array} array The array where we will use map() method.
+ * @param {Function} expression The expression that will produce elements for the new array.
+ * @param {currentValue} array[i] The current element of the array that its being processed.
+ * @param {index} i The current index inside the array.
+ * @returns {result} result[i] Result is the new array composed by the elements created through the callback function.
+ */
+
+Hooray.prototype.map = function() {
+
+	if (!(this instanceof this)) throw TypeError(array + ' NaN valid');
+	 
+	var result = [];           
+   
+	for (var i = 0; i < this.length; i++)
+   
+	  result[i] = expression(this[i], i, this);
+
+	return result;
+ 
+   }
+   
