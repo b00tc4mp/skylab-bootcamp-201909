65f59693d8999c39b987a347e68e197a465a42cb
Javier Sevilla <xaviersevilla@gmail.com>
2019-10-12 12:35:58 +0200

    update hooray

--- staff/javier-sevilla/hooray/hooray.js
+++ staff/javier-sevilla/hooray/hooray.js
@@ -2,9 +2,7 @@ function Hooray() {
     for (var i = 0; i  < arguments.length; i++)
         this[i] = arguments[i];
 
-	this.length = arguments.length;
-	
-	
+	this.length = arguments.length;	
 }
 
 /**
