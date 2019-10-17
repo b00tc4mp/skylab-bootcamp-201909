e067be9170740e98224d5d40de46b4e4ff4337ee
AdriaanZhu <qtzhuzhu@gmail.com>
2019-10-17 17:48:12 +0200

    upgrade x

--- staff/manuel-barzi/duck-app/3/scripts/main.js
+++ staff/manuel-barzi/duck-app/3/scripts/main.js
@@ -1,18 +1,21 @@
-listInitialRandomDucks();
-
-var search = new Search(document.getElementsByClassName('search')[0]);
-search.onSubmit(listSearchResults);
+listInitialRandomDucks(); // 1. IIFE
 
 function listInitialRandomDucks() {
     searchDucks('', function (ducks) {
         ducks = ducks.shuffle().splice(0, 3);
 
-        paintResults(ducks);
+        paintResults(ducks); // 2.2 quitar painResults y solo results.render(ducks)
     });
 }
 
+var search = new Search(document.getElementsByClassName('search')[0]);
+search.onSubmit(listSearchResults);  
+
+
+//
+
 function listSearchResults(query) {
-    searchDucks(query, paintResults);
+    searchDucks(query, paintResults); // 3. quitar listSearcchResults y poner solo searchDucks con una función anonima que escoge parametro de query
 }
 
 var results = new Results(document.getElementsByClassName('results')[0]);
@@ -28,5 +31,5 @@ results.onItemClick = function(duck) {
 };
 
 function paintResults(ducks) {
-    results.render(ducks);
+    results.render(ducks); // 2.1 quitar painResults y solo results.render(ducks)
 }
