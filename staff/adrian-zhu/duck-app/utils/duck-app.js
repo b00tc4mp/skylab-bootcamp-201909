28b6a1ace74e06e010dff2a1254345e24751c71b
AdriaanZhu <qtzhuzhu@gmail.com>
2019-10-17 17:49:09 +0200

    upgrade infomration

--- staff/adrian-zhu/duck-app/utils/duck-app.js
+++ staff/adrian-zhu/duck-app/utils/duck-app.js
@@ -1,12 +1,109 @@
+var xhr = new XMLHttpRequest;
+
+xhr.open('GET', 'https://duckling-api.herokuapp.com/api/search');
+
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 201) {
+        var ducks = JSON.parse(xhr.responseText);
+
+        ducks = ducks.shuffle().splice(0, 3);
+
+        var results = document.getElementsByClassName('results')[0];
+        results.innerHTML = '';
+
+        ducks.forEach(function (duck) {
+            var result = document.createElement('li');
+            result.classList.add('duck');
+            result.classList.add('duck-list');
+
+
+            var item = document.createElement('a');
+            item.classList.add("duck-list__item-link"); 
+            item.addEventListener('click', function (event) {
+                var id = duck.id;
+
+                var xhr = new XMLHttpRequest;
+
+                xhr.open('GET', 'https://duckling-api.herokuapp.com/api/ducks/' + id);
+
+                xhr.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 201) {
+                        var duck = JSON.parse(xhr.responseText);
+
+                        var detail = document.getElementsByClassName('detail')[0];
+                        detail.classList.add('duck');
+                        detail.classList.add('duck-list');
+                        
+                        var title = detail.getElementsByClassName('detail__title')[0];
+                        title.innerText = duck.title;
+                        title.classList.add("duck-list__item-title");
+
+                        var img = detail.getElementsByClassName('detail__image')[0];
+                        img.src = duck.imageUrl;
+                        img.classList.add("duck-list__item-image");
+
+                        var description = detail.getElementsByClassName('detail__description')[0];
+                        description.innerText = duck.description;
+
+                        var store = detail.getElementsByClassName('detail__store')[0];
+                        store.href = duck.link;
+
+                        var price = detail.getElementsByClassName('detail__price')[0];
+                        price.innerText = duck.price;
+                        price.classList.add("duck-list__item-price");
+
+                        var back = detail.getElementsByClassName('detail__back')[0];
+
+                        back.addEventListener('click', function (event) {
+                            var views = document.getElementsByClassName('view');
+
+                            views[0].classList.toggle('hide');
+                            views[1].classList.toggle('hide');
+                        });
+
+                        var views = document.getElementsByClassName('view');
+
+                        views[0].classList.toggle('hide');
+                        views[1].classList.toggle('hide');
+                    }
+                };
+
+                xhr.send();
+            });
+            result.append(item);
+
+            var title = document.createElement('h2');
+            title.classList.add("duck-list__item-title");
+            title.innerText = duck.title;
+
+            var img = document.createElement('img');
+            img.classList.add("duck-list__item-image");
+            img.src = duck.imageUrl;
+
+            var price = document.createElement('span');
+            price.classList.add("duck-list__item-price");
+            price.innerText = duck.price;
+
+            item.append(title, img, price);
+
+            results.append(result);
+        });
+    }
+};
+
+xhr.send();
+
+
+
 var searchFrom = document.getElementsByClassName('nav__form')[0];
 var searchInfo = document.getElementsByClassName('nav__form-search')[0];
-var main = document.getElementsByClassName('main')[0];
+var results = document.getElementsByClassName('results')[0];
 
 
 searchFrom.addEventListener('submit', function (e) {
 
     e.preventDefault();
-    main.innerHTML = ''; 
+    results.innerHTML = ''; 
 
     var xhr = new XMLHttpRequest;
     var url = 'https://duckling-api.herokuapp.com/api/search?q=' + searchInfo.value
@@ -48,7 +145,7 @@ searchFrom.addEventListener('submit', function (e) {
                 li.append(link);
                 ul.append(li);
             });
-            main.append(ul);
+            results.append(ul);
         }
     };
     xhr.send();
@@ -61,7 +158,7 @@ function detalle(id) {
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 201) {
 
-            main.innerHTML = '';
+            results.innerHTML = '';
             var duck = JSON.parse(xhr.responseText);
 
             var article = document.createElement('article');
@@ -88,12 +185,12 @@ function detalle(id) {
             boton.classList.add("duck-list__button");
 
             article.append(title, img ,description, price, boton);
-            main.append(article);
+            results.append(article);
             
         }
     }
     xhr.send();
 }
 var backPage = function() {
-    main.innerHTML = '';
+    results.innerHTML = '';
 }
