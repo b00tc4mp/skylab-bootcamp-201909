baea4a4b3bffd7270f56c86ca6569686ae15debc
RivannaDev <51270972+rivannadev@users.noreply.github.com>
2019-10-23 22:47:50 +0200

    hooray folder arrangement

--- staff/nacho-rivero/array-functions/concat/index.spec.js
+++
@@ -1,9 +0,0 @@
-describe('concat', function(){
-
-    it('should fail on undefined array', function(){
-        var array;
-
-        expect(function(){filter(array)}).toThrowError(TypeError, 'undefined is not an array');
-    });
-
-})
--- staff/nacho-rivero/array-functions/sort/sort.demo.js
+++
@@ -1,9 +0,0 @@
-console.log('DEMO: sort');
-
-var array = ['azathoth', 'yog-sothoth', 'nyarlathotep', 'shub-niggurath', 'hastur', 'dagon'];
-console.log('array', array);
-
-
-console.log('sort');
-var result = sort(array);
-console.log(result);
