a7d959d9df711d3cda7e547d4c738ff479d7a200
OscarErre <oscarr.rodfer@gmail.com>
2019-10-29 15:42:44 +0100

    add noimage

--- staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/logic/retrieve-random-beer/retrieve-random-beer.js
+++ staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/logic/retrieve-random-beer/retrieve-random-beer.js
@@ -3,7 +3,10 @@ function retrieveRandomBeer (callback) {
    //do {
         call ('GET', 'https://api.punkapi.com/v2/beers/random',undefined, undefined, (result) => {
             if (result.error) callback (new Error(result.error))
-            else callback(undefined,result[0])
+            else {
+                if (result[0].image_url === null) result[0].image_url = './img/noimage.png'
+                callback(undefined,result[0])
+            }
         })
     //    let {id, name, imageUrl, tagline} = result
    //} while (!(id && name && imageUrl && tagline))
