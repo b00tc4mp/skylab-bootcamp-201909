63ad74c79d0fb16f3da538014944506f2160b435
Jose Rodriguez <jechto610@gmail.com>
2019-10-30 22:52:28 +0100

    update logic search

--- staff/groups/groups/legends-of-react/skylol-stats/skylol-stats-app/logic/retrieve-champions/retrieveChampions.js
+++ staff/groups/groups/legends-of-react/skylol-stats/skylol-stats-app/logic/retrieve-champions/retrieveChampions.js
@@ -1,19 +1,26 @@
-function retrieveChampions(callback) {
+function retrieveChampions(query, callback) {
     if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
-    call('GET', undefined,'http://ddragon.leagueoflegends.com/cdn/9.21.1/data/en_US/champion.json', undefined, function (result) {
+    call('GET', undefined, `http://ddragon.leagueoflegends.com/cdn/9.21.1/data/en_US/champion.json`, undefined, function (result) {
         if (result.error)
             callback(new Error(result.error))
-        else{
+        else {
             result = Object.values(result.data)
             result.map(champion => {
                 champion.icon = `https://ddragon.leagueoflegends.com/cdn/9.21.1/img/champion/${champion.image.full}`
                 champion.link = `http://ddragon.leagueoflegends.com/cdn/9.21.1/data/en_US/champion/${champion.id}.json`
             })
-            
-
-            callback(undefined, result)
-            
+            if (query) {
+                query = query.charAt(0).toUpperCase() + query.slice(1)
+                const result2 = []
+                for (let i = 0; i < result.length; i++) {
+                    if (result[i].id.includes(query)) {
+                        result2.push(result[i])
+                        callback(undefined, result2)
+                    }
+                    else callback(new Error(result.error), undefined)
+                }
+            } else callback(undefined, result)
         }
     });
 }
