46d34eb473bb6cb24fd50fc9407a7c93185689fc
Jose Rodriguez <jechto610@gmail.com>
2019-10-30 22:55:55 +0100

    search

--- staff/groups/groups/legends-of-react/skylol-stats/skylol-stats-app/components/search/search.jsx
+++ staff/groups/groups/legends-of-react/skylol-stats/skylol-stats-app/components/search/search.jsx
@@ -1,4 +1,4 @@
-function Search( {error, query, onSubmit} ){
+function Search( {onSubmit, error, query} ){
     return    <section className="search">
     <h2 className="search__title">Search</h2>
     <form className="search__form" onSubmit={event => {
@@ -8,13 +8,9 @@ function Search( {error, query, onSubmit} ){
 
             onSubmit(query)
         }}>
-
-        <input type="text" className="search__field" name="query" placeholder="search" defaultValue="query"/>
+        <input type="text" className="search__field" name="query" placeholder="search" defaultValue={query}/>
         <button className="search__submit"><i className="fas fa-search"></i></button>
     </form>
     {error && <Feedback message={error} />}
-
-    {/* {results && onResultsRender(results)} */}
-
 </section>
 }
