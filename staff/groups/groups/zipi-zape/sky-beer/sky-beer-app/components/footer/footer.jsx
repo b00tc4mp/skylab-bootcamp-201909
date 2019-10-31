1266c018cde9e8b2dd4fed2aac3237d83dca4204
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2019-10-31 09:20:20 +0100

    footer and login modified

--- staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/components/footer/footer.jsx
+++ staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/components/footer/footer.jsx
@@ -5,7 +5,24 @@ function Footer () {
                 <div>Privacy Policy</div>
                 <div>Contact us</div>
                 <div>Conditions</div>
-                <div className="footer__copyright"> zipizape productions ©2019 </div>
-                <div> -- social media icons -- </div>        
+                <div className="footer__copyright"> zipizape productions ©2019 </div> 
+                <ul class="footer__socialmedia socialmedia">
+                    <li class="socialmedia__media-icon">
+                        <a href="#">
+                            <i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="socialmedia__media-icon">
+                        <a href="#">
+                            <i class="fab fa-facebook-square"></i></a>
+                    </li>
+                    <li class="socialmedia__media-icon">
+                        <a href="#">
+                            <i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li class="socialmedia__media-icon">
+                        <a href="#">
+                            <i class="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
             </footer>
 }
