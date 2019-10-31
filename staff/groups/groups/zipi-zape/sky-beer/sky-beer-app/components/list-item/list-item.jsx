845765978ca0196025c91d9dc7ff2a21307175a1
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2019-10-31 17:39:28 +0100

    modified fav icon

--- staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/components/list-item/list-item.jsx
+++ staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/components/list-item/list-item.jsx
@@ -1,14 +1,18 @@
 function ListItem ({item, onClick}) {
-    return <li className="search-results__list-item list-item" onClick={ event => {
-        event.preventDefault()
-        onClick(item.id)
-    }}>
-        <article className="search-results__inside">
-            <div className="list-item__container">
-                <h2 className="list-item__title">{item.name}</h2>
-                <img className="list-item__img" src={item.image_url}/>
-                <p className="list-item__text">{item.tagline}</p>
-            </div>
-        </article>
-    </li>
+    return <li className="search-results__list-item list-item">
+                <article className="search-results__inside">
+                    <div className="fav" onClick={ event => {
+                        event.stopPropagation()
+                        event.preventDefault()
+                    }}><i class="far fa-heart"></i></div>
+                    <div className="list-item__container" onClick={ event => {
+                        event.preventDefault()
+                        onClick(item.id)
+                    }}>
+                    <h2 className="list-item__title">{item.name}</h2>
+                    <img className="list-item__img" src={item.image_url}/>
+                    <p className="list-item__text">{item.tagline}</p>
+                </div>
+            </article>
+        </li>
 }
