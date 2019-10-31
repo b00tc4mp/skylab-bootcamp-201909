be353a474d396e1543f9995c8057979ca76cb03f
OscarErre <oscarr.rodfer@gmail.com>
2019-10-31 18:07:12 +0100

    chapu

--- staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/components/login/login.jsx
+++ staff/groups/groups/zipi-zape/sky-beer/sky-beer-app/components/login/login.jsx
@@ -2,10 +2,10 @@ function Login ({logged, name, onLogin, onRegister, onLogout, error}) {
 
     return <>
         <section className="login">
-                {!logged && <SignIn onSubmit={onLogin} /*error={error}*/ /> || <><h1>Hello {name}!</h1><a href="#" onClick={event=>{
+                {!logged && <SignIn onSubmit={onLogin} /*error={error}*/ /> || <><h1 className="login__hello-username">Hello {name}!</h1><h2 className="login__user-logout"><a href="#" onClick={event=>{
                     event.preventDefault()
                     onLogout()
-                }}>Want to LOG OUT?</a></>}
+                }}>Want to LOG OUT?</a></h2></>}
                 <SignUp onSubmit={onRegister} /*error={error}*/ />
         {error && <Feedback message={error} />}
         </section>
