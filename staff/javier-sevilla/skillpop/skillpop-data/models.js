bfdc3f022bb74d5b42db92b1083ca0357082fe52
Javier Sevilla <xaviersevilla@gmail.com>
2019-11-26 20:39:40 +0100

    api

--- staff/javier-sevilla/skillpop/skillpop-data/models.js
+++ staff/javier-sevilla/skillpop/skillpop-data/models.js
@@ -2,6 +2,5 @@ const { model } = require('mongoose')
 const { user, task } = require('./schemas')
 
 module.exports = {
-    User: model('User', user),
-    Task: model('Task', task)
+    User: model('User', user)
 }
