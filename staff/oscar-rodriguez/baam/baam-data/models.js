af0a2a172ecd054ede18ca3e7d21f6c7ba117083
OscarErre <oscarr.rodfer@gmail.com>
2019-11-26 09:03:34 +0100

    add collection model

--- staff/oscar-rodriguez/baam/baam-data/models.js
+++ staff/oscar-rodriguez/baam/baam-data/models.js
@@ -1,8 +1,9 @@
 const { model } = require ('mongoose')
-const { user, card, game } = require ('./schemas')
+const { user, card, collection, game } = require ('./schemas')
 
 module.exports = {
     User: model ('User', user),
     Card: model ('Card', card),
-    Game: model ('Game', game)
+    Game: model ('Game', game),
+    Collection: model ('Collection', collection)
 }
