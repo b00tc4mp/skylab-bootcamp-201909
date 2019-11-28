be46c24fc94c0c583ec0babfe18bcba84362309a
Isidro Alonso <isi_alonso@hotmail.com>
2019-11-28 23:34:25 +0100

    update game info icons

--- staff/isidro-alonso/gamerex-app/src/components/MyGameItem/index.js
+++ staff/isidro-alonso/gamerex-app/src/components/MyGameItem/index.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// {favourite => {if(favourite) <img src="img/favourite.png" alt="favourite" />}}
-
 export default function ({ game: {id, title, platform, sell, exchange, favourite, img} }) {
+
+    function showFav() {
+        if(favourite) return <img src="img/favourite.png" alt="favourite" />
+    }
+
+    function showSell() {
+        if(sell) return <img src="img/sell.png" alt="sell" />
+    }
+
+    function showExch() {
+        if(exchange) return <img src="img/exchange.png" alt="exchange" />
+    }
 
     return <Link to='/mygame' className="game-item__link">
         <img className="game-item__img" src={img} alt="user" />
         <p className="game-item__title">{title}</p>
         <p className="game-item__platform">{platform}</p>
-        <span className="game-item__favourite"><img src="img/favourite.png" alt="favourite" /></span>
-        <span className="game-item__sell"><img src="img/sell.png" alt="sell" /></span>
-        <span className="game-item__exchange"><img src="img/exchange.png" alt="exchange" /></span>
+        <span className="game-item__favourite">{showFav()}</span>
+        <span className="game-item__sell">{showSell()}</span>
+        <span className="game-item__exchange">{showExch()}</span>
     </Link>
 }
