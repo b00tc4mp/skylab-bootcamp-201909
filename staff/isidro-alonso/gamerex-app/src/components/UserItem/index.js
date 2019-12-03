be2eccd12b79751902f9d21bf89d164e928442ce
Isidro Alonso <isi_alonso@hotmail.com>
2019-12-03 10:04:04 +0100

    update userlist links

--- staff/isidro-alonso/gamerex-app/src/components/UserItem/index.js
+++ staff/isidro-alonso/gamerex-app/src/components/UserItem/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { retrieveMyUser } from '../../logic'
 
-export default function ({ user: {_id: id, username, img} }) {
-    const { token } = sessionStorage;
+export default withRouter(function ({ user: {_id: userId, username, img} }) {
+    const [id, setId] = useState()
 
-    const userLink = token? `/userinfo/${id}`:'/login';
+    useEffect(() => {
+        const { token } = sessionStorage;
+
+        (async () => {
+            if (token) {
+                const { id } = await retrieveMyUser(token)
+
+                setId(id)
+
+            }
+        })()
+    }, [sessionStorage.token])
+
+    const userLink = (userId !== id)? `/userinfo/${userId}`:'/myuser';
 
     return <Link to={userLink} className="user-item__link" >
         <img className="user-item__img" src={img} alt="user" />
         <p className="user-item__username">{username}</p>
     </Link>
-}
+})
