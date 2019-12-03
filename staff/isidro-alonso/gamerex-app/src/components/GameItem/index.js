3e91b87ec56f295253a4c28f8bde691f3d28210b
Isidro Alonso <isi_alonso@hotmail.com>
2019-12-03 22:19:34 +0100

    style update

--- staff/isidro-alonso/gamerex-app/src/components/GameItem/index.js
+++ staff/isidro-alonso/gamerex-app/src/components/GameItem/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function ({ game: {_id: id, title, platform, sell, exchange, favourite} }) {
+export default function ({ game: { _id: id, title, platform, sell, exchange, favourite } }) {
 
     function showFav() {
-        if(favourite) return <img src="img/favourite.png" alt="favourite" />
+        if (favourite) return <img src="img/favourite.png" alt="favourite" />
     }
 
     function showSell() {
-        if(sell) return <img src="img/sell.png" alt="sell" />
+        if (sell) return <img src="img/sell.png" alt="sell" />
     }
 
     function showExch() {
-        if(exchange) return <img src="img/exchange.png" alt="exchange" />
+        if (exchange) return <img src="img/exchange.png" alt="exchange" />
     }
 
     const userGameLink = `/usergame/${id}`
@@ -20,13 +20,11 @@ export default function ({ game: {_id: id, title, platform, sell, exchange, favo
     const img = 'img/nodata.png'
 
     return <Link to={userGameLink} className="game-item__link">
-        <section className="game-item">
-            <img className="game-item__img" src={img} alt="game" />
-            <p className="game-item__title">{title}</p>
-            <p className="game-item__platform">{platform}</p>
-            <span className="game-item__favourite">{showFav()}</span>
-            <span className="game-item__sell">{showSell()}</span>
-            <span className="game-item__exchange">{showExch()}</span>
-        </section>
+        <img className="game-item__img" src={img} alt="game" />
+        <p className="game-item__title">{title}</p>
+        <p className="game-item__platform">{platform}</p>
+        <span className="game-item__favourite">{showFav()}</span>
+        <span className="game-item__sell">{showSell()}</span>
+        <span className="game-item__exchange">{showExch()}</span>
     </Link>
 }
