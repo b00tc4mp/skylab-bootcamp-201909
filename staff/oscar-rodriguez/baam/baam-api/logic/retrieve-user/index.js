f901d895471d9f9aa5bb7cc40cd544e0ad3126b1
OscarErre <oscarr.rodfer@gmail.com>
2019-12-04 17:46:54 +0100

    add stats to retrieveUser

--- staff/oscar-rodriguez/baam/baam-api/logic/retrieve-user/index.js
+++ staff/oscar-rodriguez/baam/baam-api/logic/retrieve-user/index.js
@@ -15,8 +15,8 @@ module.exports = function (id) {
 
         await user.save()
 
-        const { name, surname, email, nickname, lastAccess } = user.toObject()
+        const { name, surname, email, nickname, lastAccess, stats } = user.toObject()
 
-        return { id, name, surname, email, nickname, lastAccess }
+        return { id, name, surname, email, nickname, lastAccess, stats }
     })()
 }
