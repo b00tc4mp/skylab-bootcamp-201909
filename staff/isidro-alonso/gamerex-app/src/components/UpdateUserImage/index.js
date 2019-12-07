0adeb1a667d2827b9ef67b91d0b6e1e6da244dd3
Isidro Alonso <isi_alonso@hotmail.com>
2019-12-07 17:27:19 +0100

    update error feedback

--- staff/isidro-alonso/gamerex-app/src/components/UpdateUserImage/index.js
+++ staff/isidro-alonso/gamerex-app/src/components/UpdateUserImage/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { saveImageUser } from '../../logic'
 import { withRouter } from 'react-router-dom'
 import Feedback from '../Feedback'
